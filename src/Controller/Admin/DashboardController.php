573e9098a8b7e8db9d243cc3e9ebb7de47290847
Chiara <ginelli.ch@gmail.com>
2026-02-19 12:23:02 +0100

    Ajout du CRUD

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,6 +7,11 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Editor;
+use App\Entity\Game;
+use App\Entity\Genre;
+use App\Entity\Review;
+use App\Entity\User;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
@@ -25,7 +30,12 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-newspaper');
+        yield MenuItem::linkToCrud('Editor', 'fas fa-list', Editor::class);
+        yield MenuItem::linkToCrud('Game', 'fas fa-list', Game::class);
+        yield MenuItem::linkToCrud('Genre', 'fas fa-list', Genre::class);
+        yield MenuItem::linkToCrud('Review', 'fas fa-list', Review::class);
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
     }
+
 }
