17c09d55864f26d8619d17ead419508ddc439459
Chiara <ginelli.ch@gmail.com>
2026-02-19 16:20:55 +0100

    Version basique

--- src/Entity/Review.php
+++ src/Entity/Review.php
@@ -6,6 +6,7 @@ use App\Repository\ReviewRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\HasLifecycleCallbacks]
 #[ORM\Entity(repositoryClass: ReviewRepository::class)]
 class Review
 {
@@ -30,6 +31,15 @@ class Review
     #[ORM\ManyToOne(inversedBy: 'reviews')]
     #[ORM\JoinColumn(nullable: false)]
     private ?Game $game = null;
+
+    #[ORM\PrePersist]
+    public function setCreatedAtValue(): static
+    {
+        $this->createdAt = new \DateTimeImmutable();
+
+
+        return $this;
+    }
 
     public function getId(): ?int
     {
